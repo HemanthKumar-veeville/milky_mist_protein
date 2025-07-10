0e796deb5f2758a20ce6293192caa148afafb970
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2025-07-10 23:22:35 +0530

    Update colours

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -7,15 +7,17 @@ export const Layout = () => {
   const [selectedState, setSelectedState] = useState<string>("Karnataka");
 
   return (
-    <div className="min-h-screen max-w-[390px] w-full mx-auto">
-      <Header
-        selectedState={selectedState}
-        setSelectedState={setSelectedState}
-        states={STATES}
-      />
-      <main className="pt-[50px] sm:pt-[65px]">
-        <Outlet context={{ selectedState, setSelectedState }} />
-      </main>
+    <div className="bg-[#C0C9EE] min-h-screen w-full">
+      <div className="min-h-screen max-w-[390px] w-full mx-auto bg-white">
+        <Header
+          selectedState={selectedState}
+          setSelectedState={setSelectedState}
+          states={STATES}
+        />
+        <main className="pt-[50px] sm:pt-[65px]">
+          <Outlet context={{ selectedState, setSelectedState }} />
+        </main>
+      </div>
     </div>
   );
 };
